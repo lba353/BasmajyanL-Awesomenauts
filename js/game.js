109b59e5dbd1949385437ce9adc6b0f132a63636
Ludvig Basmajyan <lbasmajyan@gmail.com>
2015-03-09 10:46:38 -0700

    Awesomenauts (End of 3/9)
    Making the bases.

--- js/game.js
+++ js/game.js
@@ -40,6 +40,8 @@ var game = {
 	// Run on game resources loaded.
 	"loaded" : function () {
                 me.pool.register("player", game.PlayerEntity, true);
+                me.pool.register("playerBase", game.PlayerBaseEntity);
+                me.pool.register("enemyBase", game.EnemyBaseEntity);
             
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
